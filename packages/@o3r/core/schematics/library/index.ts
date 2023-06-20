27fff66984104517a6158cd90d9e619e1df1f62b
Kilian Panot <kilian.panot@amadeus.com>
2023-06-20 12:09:36 +0900

    feat: base the module generation on Angular cli library generation

---
+++ packages/@o3r/core/schematics/library/index.ts
@@ -0,0 +1,38 @@
+import { chain, noop, Rule, schematic } from '@angular-devkit/schematics';
+import * as path from 'node:path';
+import { applyEsLintFix, isNxContext } from '@o3r/schematics';
+import { NgGenerateModuleSchema } from './schema';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { nxGenerateModule } from './rules/rules.nx';
+import { ngGenerateModule } from './rules/rules.ng';
+
+/**
+ * Add an Otter compatible module to a monorepo
+ *
+ * @param options
+ * @param rootPath
+ */
+export function generateModule(options: NgGenerateModuleSchema): Rule {
+
+  /** Name of the Nx Project in case of Nx monorepo */
+  const projectName = options.projectName || options.name.replace(/^@/, '').replace('/', '-');
+
+  return (tree, context) => {
+    const isNx = isNxContext(tree);
+
+    const defaultRoot = isNx && (tree.readJson('/nx.json') as any)?.workspaceLayout?.libsDir || 'packages';
+
+    /** Path to the folder where generate the new module */
+    const targetPath = path.posix.resolve('/', options.path || defaultRoot, options.name);
+    const extendedOptions = { ...options, targetPath, projectName };
+
+    return chain([
+      isNx ? nxGenerateModule(options) : ngGenerateModule(extendedOptions),
+      (t, c) => schematic('ng-add-create', { projectName, path: targetPath })(t, c), options.skipLinter ? noop() : applyEsLintFix(),
+      options.skipInstall ? noop() : (t, c) => {
+        c.addTask(new NodePackageInstallTask());
+        return t;
+      }
+    ])(tree, context);
+  };
+}
